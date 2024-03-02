185123c7878a7f0280268c6a33fddb1c8d8aecfb
robac <robert@poch.cz>
2024-03-02 19:24:01 +0100

    cahcing results

--- main.py
+++ main.py
@@ -26,10 +26,14 @@ def aggrid_interactive_table(df: pd.DataFrame):
         theme="alpine",
         update_mode=GridUpdateMode.MODEL_CHANGED,
         allow_unsafe_jscode=True,
-        width=1000
     )
 
     return selection
+
+
+@st.cache_data
+def load_results(text):
+    return search_ins.search([text])
 
 search_ins = ConsulSearch()
 search_ins.load_data(get_config())
@@ -39,7 +43,7 @@ title = st.text_input('Text', '')
 keys = st.checkbox('search in keys', value=True)
 values = st.checkbox('search in values', value=True)
 if len(title) > 0:
-    data = search_ins.search([title])
+    data = load_results(title)
     if len(data) > 0:
         selection = aggrid_interactive_table(data)
         if selection and len(selection["selected_rows"]) > 0:
