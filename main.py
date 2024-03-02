3d09d548db1f4f3bae4b51c1e344e9944c74a772
robac <robert@poch.cz>
2024-03-02 15:20:26 +0100

    rewrite to Streamlit initial commit

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
-from pages import *
-from nicegui import app, ui
+import pandas as pd
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+from consul_tools.ConsulSearch import ConsulSearch
 
-@ui.page('/')
-def route_search() -> None:
-    search.content()
+def get_config():
+    return {
+        "CONSUL_PATH" : r"consul.consul-1.cecolo.gofg.net",
+        "CONSUL_PORT" : 8500,
+        "CONSUL_ENCODING" : "UTF-8",
+        "SEARCH_RECURSE" : True,
+        "SEARCH_INDEX" : ""
+    }
 
-ui.run()
+def aggrid_interactive_table(df: pd.DataFrame):
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+
+    options.configure_side_bar()
+
+    options.configure_selection("single")
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="alpine",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True,
+    )
+
+    return selection
+
+
+st.header('Consul search', divider='blue')
+title = st.text_input('Text', '')
+keys = st.checkbox('search in keys', value=True)
+values = st.checkbox('search in values', value=True)
+if len(title) > 0:
+    search_ins = ConsulSearch()
+    search_ins.load_data(get_config())
+    data = search_ins.search([title])
+    df = pd.DataFrame(data=data)
+    selection = aggrid_interactive_table(df)
+    st.write("hotovo")
