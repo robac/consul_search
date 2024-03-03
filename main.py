b975f8854f87f7f62b101237d499ce0455dbd5b2
robac <robert@poch.cz>
2024-03-03 18:02:40 +0100

    changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode
 from consul_tools.ConsulSearch import ConsulSearch
+from consul_tools import utils
 
 def get_config():
     return {
@@ -19,35 +20,38 @@ def aggrid_interactive_table(df: pd.DataFrame):
     )
 
     options.configure_selection("single")
+    options.configure_auto_height(autoHeight=False)
     selection = AgGrid(
         df,
         enable_enterprise_modules=True,
         gridOptions=options.build(),
-        theme="alpine",
+        theme="material",
         update_mode=GridUpdateMode.MODEL_CHANGED,
         allow_unsafe_jscode=True,
+        height=500
     )
 
     return selection
 
 
 @st.cache_data
-def load_results(text):
-    return search_ins.search([text])
+def load_results(text, search_keys, search_values):
+    return search_ins.search([text], search_keys, search_values)
 
 search_ins = ConsulSearch()
 search_ins.load_data(get_config())
 
+st.set_page_config(layout="wide")
 st.header('Consul search', divider='blue')
 title = st.text_input('Text', '')
 keys = st.checkbox('search in keys', value=True)
 values = st.checkbox('search in values', value=True)
 if len(title) > 0:
-    data = load_results(title)
-    if len(data) > 0:
+    data = load_results(title, keys, values)
+    if data is not None and len(data) > 0:
         selection = aggrid_interactive_table(data)
         if selection and len(selection["selected_rows"]) > 0:
-            st.write(selection["selected_rows"][0]["key"])
-            st.write(selection["selected_rows"][0]["value"])
+            st.write(utils.get_consul_kv_path(get_config(), selection["selected_rows"][0]["key"]))
+            st.code(selection["selected_rows"][0]["value"])
     else:
         st.write("**no data**")
