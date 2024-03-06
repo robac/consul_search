3654506dc91df005fa3b3f7d27f7fce2cb0cd863
robac <robert@poch.cz>
2024-03-06 17:32:38 +0100

    refactor - modify, decode data on load, exclusion made also suring load

--- consul_tools/ConsulSearch.py
+++ consul_tools/ConsulSearch.py
@@ -12,7 +12,6 @@ class SearchOptions:
         self.SEARCH_VALUES = search_values
         self.INDEX = index
         self.REGULAR = regular
-
     def __reduce__(self):
         return (self.SEARCH_KEYS, self.SEARCH_VALUES, self.REGULAR, self.INDEX)
 
@@ -30,34 +29,35 @@ class ConsulSearch:
         self.consul_data = None
         self.config = None
         self.sections = None
+        self.excludes = None
 
     @property
     def can_process(self):
         return self.last_update and not self.in_progress
 
     def load_sections(self):
-        sections = defaultdict()
-        sections[""] = None
-        for item in self.consul_data:
-            key = item["Key"]
-            path = key.split("/")
-            if len(path) > 1:
-                section = '/'.join(path [0:-1])
-                sections[section] = None
-        self.sections = sections
+        pass
 
     def load_data(self, config):
         print("Load started")
         self.config = config
-        print(config)
+        self.excludes = self._prepare_excludes()
+
         consul_instance = consul.Consul(host=self.config["host"], port=self.config["port"])
-        #try:
-        self.consul_index, self.consul_data = consul_instance.kv.get("", recurse=True)
-        self.last_update = datetime.datetime.now()
-        self.load_sections()
-        #except Exception as e:
-        #    self.consul_index, self.consul_data, self.last_update = (None, None, None)
-        #    print(e)
+        data = defaultdict()
+        try:
+            self.consul_index, consul_data = consul_instance.kv.get("", recurse=True)
+            self.last_update = datetime.datetime.now()
+            self.load_sections()
+            for item in consul_data:
+                if self.excludes is not None and re.match(self.excludes, item['Key']):
+                    print("excluded {item['Key']}")
+                else:
+                    data[item["Key"]] = item["Value"].decode(config["encoding"]) if ("Value" in item and item['Value'] is not None) else None
+            self.consul_data = data
+        except Exception as e:
+            self.consul_index, self.consul_data, self.last_update = (None, None, None)
+            print(e)
         print("load finished")
         print(f"index: {self.consul_index}")
         print(len(self.consul_data) if self.consul_data else 0)
@@ -74,31 +74,26 @@ class ConsulSearch:
             print(f'CONFIG: installed pattern {pattern}')
         return result
 
-    def _search_item(self, item, searches, excludes, options : SearchOptions):
+    def _search_item(self, item, searches, options : SearchOptions):
         """
         Search in one item for match with 'searches'.
         :param item: item to process
         :param searches: list of searched patterns
         :return: ((search_found_in_key, search_found_in_value), key, value)
         """
-        encoding = self.config["encoding"]
-        if excludes is not None and re.match(excludes, item['Key']):
-            return ((False, False), None)
-            print("excluded {item['Key']}")
-
-        value = item['Value'].decode(encoding) if (('Value' in item) and item['Value'] is not None) else None
+        key, value = item
 
         found_key = False
         found_value = False
         for s in searches:
-            if options.SEARCH_KEYS and s.search(item['Key']):
+            if options.SEARCH_KEYS and s.search(key):
                 found_key = True
             if options.SEARCH_VALUES and (value is not None) and s.search(value):
                 found_value = True
 
-        return ((found_key, found_value), item['Key'], value)
+        return ((found_key, found_value), key, value)
 
-    def _search_items(self, searches, excludes, options : SearchOptions):
+    def _search_items(self, searches, options : SearchOptions):
         print(f"Search start. Data version: {self.last_update}")
         df_structure = {
             'inkey': [],
@@ -109,8 +104,8 @@ class ConsulSearch:
 
         results = pd.DataFrame(df_structure)
         count = 0
-        for item in self.consul_data:
-            res = self._search_item(item, searches, excludes, options)
+        for key, value in self.consul_data.items():
+            res = self._search_item((key, value), searches, options)
             if (res[0][0] or res[0][1]):
                 new_row = {'inkey': res[0][0], 'invalue': res[0][1], 'key': res[1], 'value' : res[2]}
                 results.loc[len(results)] = new_row
@@ -127,13 +122,11 @@ class ConsulSearch:
             lst.append(pattern1)
             lst.append(pattern2)
 
-        return "|".join(lst)
+        return re.compile("|".join(lst))
 
     def search(self, searches, options : SearchOptions):
         if self.consul_data is None:
             return None
 
         searches = self._prepare_searches(searches, options)
-        excludes = self._prepare_excludes()
-        print (f"Excludes {excludes}")
-        return self._search_items(searches, excludes, options)
+        return self._search_items(searches, options)
